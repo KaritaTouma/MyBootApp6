803b1fcc5ee807818c106bb58b910ba68af5c051
190103 <190103@e0601-09.JC-21.local>
2022-06-10 14:53:58 +0900

    MyBootApp9コミット

---
+++ src/main/java/jp/te4a/spring/boot/myapp9/BookService.java
@@ -0,0 +1,68 @@
+package jp.te4a.spring.boot.myapp9;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import jp.te4a.spring.boot.myapp9.BookBean;
+import jp.te4a.spring.boot.myapp9.BookForm;
+@Service
+public class BookService {
+	@Autowired
+	BookRepository bookRepository;
+	  
+	public BookForm create(BookForm bookForm) {
+		  BookBean bookBean = new BookBean();
+		  BeanUtils.copyProperties(bookForm, bookBean);
+		  bookRepository.save(bookBean);
+		  return bookForm;
+	}
+	  
+	public BookForm update(BookForm bookForm) {
+		  BookBean bookBean = new BookBean();
+		  BeanUtils.copyProperties(bookForm, bookBean);
+		  bookRepository.save(bookBean);
+		  return bookForm;
+	}
+	
+	public void delete(Integer id) {
+		BookBean bookBean = new BookBean();
+		bookBean.setId(id);
+//		BeanUtils.copyProperties(a, bookBean);
+//		bookBean.setPrice(a.getPrice());
+//		bookBean.setPublisher(a.getPublisher());
+//		bookBean.setTitle(a.getTitle());
+//		bookBean.setWritter(a.getWritter());
+		
+		bookRepository.delete(bookBean);
+
+		};
+
+
+	
+	
+	public List<BookForm> findAll() {
+		List<BookBean> beanList = bookRepository.findAll();
+		List<BookForm> formList = new ArrayList<BookForm>();
+		for(BookBean bookBean: beanList) {
+			BookForm bookForm = new BookForm();
+			BeanUtils.copyProperties(bookBean, bookForm);
+			formList.add(bookForm);
+		}
+		return formList;
+	}
+	
+	public BookForm findOne(Integer id) {
+		BookForm bookForm = new BookForm();
+		
+		//BookBean bookBean = bookRepository.findById(int);
+		Optional<BookBean> opt = bookRepository.findById(id);
+		opt.ifPresent(book -> {
+			BeanUtils.copyProperties(book, bookForm);
+		});
+		
+		return bookForm;
+	}
+}
